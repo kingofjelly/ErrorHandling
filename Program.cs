90fe06b498fe1cd052860b81e8374622798c5563
JellyKing <curriedmc@gmail.com>
2015-08-24 16:32:40 +0100

    First commit of all documents

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ErrorHandling
+{
+    class Program
+    {
+
+        public static string G_errorMessage;//this is a global variable which will be used to hold the error message, opposed to passing it down all the layers
+        //look into public, static, private
+        //incorporate enums
+        //figure out procedure to hook in my exception method
+
+        static void Main(string[] args)
+        {
+            //this is an application i'm making in order to handle exceptions and relay to an error file. if the file doesnt exist, it will be created
+            getUserInput();//run method below
+        }
+
+        public static void getUserInput()
+        {
+            //1: Ask question, then get user input. Wrong data will intentionally be entered, to trip an error and look at error handling. Investigate try.parse too and try catch.
+            int userNumber;//for user age. will be entering strings to trip and error
+            string userInput;//used for any parsing needed.
+
+            exceptionEncounteredHandling errorHandling = new exceptionEncounteredHandling();
+            
+            Console.WriteLine("enter a numerical number");
+            userInput = Console.ReadLine();
+           
+                //wrap this within try parse/try catch?
+                try
+                {
+                    
+                    userNumber = int.Parse(userInput);
+                    Console.WriteLine("The number is: " + userNumber + "?\nIs this correct?");
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message.ToString());//this needs to be passed down the multiple layers OR stored globally. globally is probably best bet
+                    G_errorMessage = ex.Message.ToString();
+                    Console.WriteLine("Error collecting your input. Will attempt to write to log file");
+                    //this is where it calls a seperate method nested here. it will check for a folder in the default location. if not create it and state whether or not it has
+
+                    //run the error class here. pass in the error message.
+
+                    errorHandling.handleErrors(ex.Message.ToString());//call my error handling class
+                    
+                }
+
+              
+            
+        }
+
+       
+    }
+}
---
+++ exceptionEncounteredHandling.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ErrorHandling
+{
+    class exceptionEncounteredHandling
+    {
+        /* This is a special method i've created, to be able to interact with it in other applications. 
+         * This method is used to handle exceptions and write to a log file.
+         * The aim of this is to be able to export it to whatever application I make
+         * It will be continually tweaked and developed in order to handle exceptions according to their type.         
+         */
+
+
+        /*
+         * Goals of this class:
+         * -Use enmums
+         * -Use boolean triggers
+         * -Investigate and use if possible try catch + throw (bubble up)
+         * -Enable for general use in future applications
+         * -Eventually package to .DLL and test?
+         * -Try switching
+         */
+        //private string G_errorMessage;//global variable, formed from string parsed from method calling this class
+
+        private bool G_folderPresent = false;//boolean to be tripped if folder is present. Static, as after set, it will remain until program rebooted. Not reset each time method called.
+
+        private bool G_filePresent = false;//boolean to be tripped if file is present. Static, as after set, it will remain until program rebooted. Not reset each time method called.
+
+        /*This is a global variable which will be used to hold the error message, opposed to passing it down all the layers
+         *If it was the same error message for everything, it would be static. We just want it accessible in this method, however it isn't constant          
+         *That means it isn't static.
+         */
+
+       
+
+        
+
+        public void handleErrors(string errorMessage)
+        {
+            /*This method is the one which kicks off the error handling. It will be called, after being istantiated through the parent class & method, then
+             *the error message is passed down, into this method, where it's then stored in a global variable.
+             *This global variable is then used throughout.
+             */
+
+            //G_errorMessage = errorMessage;//set the parsed variable to the global one.
+
+            //MAKE A CONSTRUCTOR CLASS. RUNS THIS EVERY TIME, SO THAT BOOLEANS ARE PRE-SET?
+
+            if (G_folderPresent && G_filePresent)
+            {
+                //as both are present, skip to method which writes to file.
+                writeToErrorLog(errorMessage);//pass in errorMessage. skip global
+            }
+            else
+            {
+                checkForFolder(errorMessage);//pass in errorMessage. skip global
+                /*if not. check for folder and work down tree to check whether file is there. Create what's missing, then set booleans showing they're there, to bypass
+                 *this upon the next run.
+                 */
+            }
+           
+
+
+        }
+
+        private void checkForFolder(string errorMessage)
+        {
+            //check default area for folder to store error log in. if not, create one.
+            //give feedback to user as to whether one exists or has been created.
+
+            //IF DIRECTORY EXISTS, INFORM USER FOR TEST PURPOSES. IF DOESN'T EXIST, OUTPUT RELEVANT MESSAGE.
+            if (Directory.Exists("C:\\tmp"))
+            {
+                Console.WriteLine("Error Folder Exists");
+                //NOW GO INTO FOLDER LOOKING FOR RELEVANT ERROR FILE
+                G_folderPresent = true;
+                //checkForErrorLog();//don't need this. writing creates if not present.
+                writeToErrorLog(errorMessage);
+            }
+            else
+            {
+                Console.WriteLine("Doesn't Exist, \nCreating folder for you now");
+                createFolder();
+                //CREATE FOLDER
+                //createErrorLog();
+                //you don't need to check for the log, as it's not there. no folder = no file under that directory.
+                writeToErrorLog(errorMessage);//by this point both booleans should be true
+            }
+        }
+
+        private void createFolder()
+        {
+            //this method creates the folder for the user.
+            try
+            {
+                //try creating folder for user
+                Directory.CreateDirectory("C:\\tmp");
+                Console.WriteLine("Error Directory Created");
+                G_folderPresent = true;
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                Console.WriteLine("Error Creating Error Directory Folder");
+                //error
+            }
+        }
+        //THE TWO METHODS BELOW ARE NOT NEEDED. LOGS ARE AUTOMATICALLY CREATED
+        //private void checkForErrorLog()
+        //{
+        //    //this method is used for check whether the error log file exists. this will be called if the folder exists. if it doesn't file will be assumed missing and created.
+        //    //need to check if it exists. if so is it read only/open? if so throw error. explaining situation
+        //    if (File.Exists(@"C:\tmp\errors.txt"))
+        //    {
+        //        Console.WriteLine("Log file exists. Will now write to it");
+        //        G_filePresent = true;
+                
+                
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Log file not present. Now Creating it");
+        //        createErrorLog();
+        //    }
+
+        //}
+
+        //public static void createErrorLog()
+        //{
+        //    //this method will create the error log. it will auto create it if it's creating the tmp folder, as it won't be present. It will also create it if it has examined
+        //    //the tmp folder and found it to be not present.
+        //    string path = @"C:\tmp\errors.txt";
+
+        //    try
+        //    {
+        //        TextWriter tw = new StreamWriter(path, true);
+        //        // tw.WriteLine("This is an error line!");
+        //        tw.Close();
+        //        // Console.WriteLine("written to error log");
+        //        Console.WriteLine("Error file created");
+
+        //        G_filePresent = true;
+                
+
+        //        //write to it here
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("Error creating error log file");
+        //        Console.WriteLine(ex.Message.ToString());
+
+        //    }
+        //}
+
+        private static void writeToErrorLog(string errorMessage)
+        {
+            //writes to the rror log, created above.
+            string path = @"C:\tmp\errors.txt";
+            string timeNow = DateTime.Now.ToString("MM/dd/yyyy h:mm tt");
+            try
+            {
+                using (FileStream fs = new FileStream(path,FileMode.Append,FileAccess.Write))
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    //format entry to log file here.
+                    //I want Error + error message + Date/Time stamp
+                    sw.WriteLine("Error:        " + errorMessage + "      " + timeNow);
+                }
+                Console.WriteLine("Successfully written to error log file");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error encountered writing to error file. Oh the irony.");
+                
+                Console.WriteLine(ex.Message.ToString());
+
+
+            }
+        }
+    }
+}
